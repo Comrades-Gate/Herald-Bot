b846d3ba5cbedd6992198951ed56df3b2625f2fe
Xxscorchxx5 <>
2021-05-09 17:50:03 +0000

    - The universal command prefix for Herald Bot is /hb
    - Added a suggestion command that is invoked with /hbsuggest <your_suggestion>
    
    - Reformatted quotes that are returned with /hbquote
    
    - Added an motivational message command that is invoked with /hbisad

--- main.py
+++ main.py
@@ -4,46 +4,42 @@ import requests
 import json
 import random
 from keep_alive import keep_alive
+from discord.ext import commands
 
-client = discord.Client()
-
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing", "fml", "kms"]
-
-starter_encouragements = [
-  "Hang in there!",
-  "You are precious cargo!",
-  "I think you're pretty great!",
-  "ily <3"
-]
+my_secret = os.environ['TOKEN'] # Encrypted bot invocation token.
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  quote = json_data[0]['q'] + " **- "+ json_data[0]['a'] + "**"
   return quote
 
-@client.event
+bot = commands.Bot(command_prefix='/hb')
+
+@bot.event
 async def on_ready():
-    print('{0.user} is now online.'.format(client))
+  print(f'{bot.user.name} is now online.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def suggest(ctx, *, arg):
+  embed = discord.Embed(title='Herald Bot Suggestions', description=f'Submitted by: {ctx.author.mention}', color=discord.Color.green())
+  embed.add_field(name='Suggestion', value=arg)
+  channel = ctx.guild.get_channel(840961608166801458)
+  msg = await channel.send(embed=embed)
+  await msg.add_reaction('👍') # yea
+  await msg.add_reaction('👎') # nay
+  await ctx.message.delete()
 
-    msg = message.content
+@bot.command()
+async def quote(message):
+  q = get_quote()
+  await message.channel.send(q)
 
-    if msg.startswith('/help'):
-        await message.channel.send('IM NOT FINISHED YET')
-    
-    if msg.startswith('/quote'):
-      quote = get_quote()
-      await message.channel.send(quote)
-    
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice
-      (starter_encouragements))
+starter_encouragements = ["Hang in there!","You are precious cargo!","I think you're pretty great!","ily <3","You can do it booboo!","I am rooting for you!"]
+@bot.command()
+async def isad(message):
+  await message.channel.send(random.choice(starter_encouragements))
 
 keep_alive()
 my_secret = os.environ['TOKEN']
-client.run(my_secret)
+bot.run(my_secret)

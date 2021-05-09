e819581419e402551da4f70750ed6a2a2dacd55b
Scerch <83891495+Scerch@users.noreply.github.com>
2021-05-08 23:46:23 -0400

    Initial upload of main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('216!'):
+        await message.channel.send('IM NOT FINISHED YET')
+
+client.run(os.getenv('TOKEN'))
